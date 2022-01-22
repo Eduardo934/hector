9773e9b3dc801f547cf2b8a98226ee388ebb1a39
Eduardo934 <64944080+Eduardo934@users.noreply.github.com>
2022-01-21 20:04:11 -0600

    Update app.R

--- app.R
+++ app.R
@@ -1,36 +1,13 @@
 library(shiny)
 library(shinyFiles)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage( # Application title
-  mainPanel(
-    shinyDirButton("dir", "Input directory", "Upload"),
-    verbatimTextOutput("dir", placeholder = TRUE)  # added a placeholder
-  ))
-
-server <- function(input, output) {
-  shinyDirChoose(
-    input,
-    'dir',
-    roots = c(home = '~'),
-    filetypes = c('', 'txt', 'bigWig', "tsv", "csv", "bw")
-  )
-
-  dir <- reactive(input$dir)
-  output$dir <- renderText({  # use renderText instead of renderPrint
-    parseDirPath(c(home = '~'), dir())
+ui <- fluidPage(
+  shinyDirButton('folder', 'Select a folder', 'Please select a folder', FALSE)
+)
+server <- function(input, output){
+  volumes = getVolumes() # this makes the directory at the base of your computer.
+  observe({
+    shinyDirChoose(input, 'folder', roots=c('wd' = '.'), filetypes=c('', 'txt'))
   })
-
-  observeEvent(ignoreNULL = TRUE,
-               eventExpr = {
-                 input$dir
-               },
-               handlerExpr = {
-                 home <- normalizePath("~")
-                 datapath <<-
-                   file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
-               })
 }
-
-# Run the application
-shinyApp(ui = ui, server = server)
+shinyApp(ui=ui, server=server)
