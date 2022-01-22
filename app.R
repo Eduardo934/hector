db5b0010745eb5b7c586ef98b7b0aec2d475fb2d
Eduardo934 <64944080+Eduardo934@users.noreply.github.com>
2022-01-21 19:55:13 -0600

    Update app.R

--- app.R
+++ app.R
@@ -1,10 +1,11 @@
 library(shiny)
 library(shinyFiles)
 
+# Define UI for application that draws a histogram
 ui <- fluidPage( # Application title
   mainPanel(
     shinyDirButton("dir", "Input directory", "Upload"),
-    verbatimTextOutput("dir", placeholder = TRUE)  
+    verbatimTextOutput("dir", placeholder = TRUE)  # added a placeholder
   ))
 
 server <- function(input, output) {
@@ -14,23 +15,19 @@ server <- function(input, output) {
     roots = c(home = '~'),
     filetypes = c('', 'txt', 'bigWig', "tsv", "csv", "bw")
   )
-  
-  global <- reactiveValues(datapath = getwd())
-  
+
   dir <- reactive(input$dir)
-  
-  output$dir <- renderText({
-    global$datapath
+  output$dir <- renderText({  # use renderText instead of renderPrint
+    parseDirPath(c(home = '~'), dir())
   })
-  
+
   observeEvent(ignoreNULL = TRUE,
                eventExpr = {
                  input$dir
                },
                handlerExpr = {
-                 if (!"path" %in% names(dir())) return()
                  home <- normalizePath("~")
-                 global$datapath <-
+                 datapath <<-
                    file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
                })
 }
