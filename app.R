0294ebef30b2fac6f1367f3b314353c07f7aef1c
Eduardo934 <64944080+Eduardo934@users.noreply.github.com>
2022-01-21 19:42:31 -0600

    Add files via upload

---
+++ app.R
@@ -0,0 +1,13 @@
+library(shiny)
+library(shinyFiles)
+
+ui <- fluidPage(
+    shinyDirButton('folder', 'Select a folder', 'Please select a folder', FALSE)
+)
+server <- function(input, output){
+    volumes = getVolumes() # this makes the directory at the base of your computer.
+    observe({
+        shinyDirChoose(input, 'folder', roots=volumes(), filetypes=c('', 'txt'))
+    })
+}
+shinyApp(ui=ui, server=server)
