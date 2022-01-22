bc8b3c0c3dae7f53f9a5c810de22ae2bf8550d36
Eduardo934 <64944080+Eduardo934@users.noreply.github.com>
2022-01-21 19:49:52 -0600

    Update app.R

--- app.R
+++ app.R
@@ -1,13 +1,39 @@
 library(shiny)
 library(shinyFiles)
 
-ui <- fluidPage(
-    shinyDirButton('folder', 'Select a folder', 'Please select a folder', FALSE)
-)
-server <- function(input, output){
-    volumes = getVolumes() # this makes the directory at the base of your computer.
-    observe({
-        shinyDirChoose(input, 'folder', roots=volumes(), filetypes=c('', 'txt'))
-    })
+ui <- fluidPage( # Application title
+  mainPanel(
+    shinyDirButton("dir", "Input directory", "Upload"),
+    verbatimTextOutput("dir", placeholder = TRUE)  
+  ))
+
+server <- function(input, output) {
+  shinyDirChoose(
+    input,
+    'dir',
+    roots = c(home = '~'),
+    filetypes = c('', 'txt', 'bigWig', "tsv", "csv", "bw")
+  )
+  
+  global <- reactiveValues(datapath = getwd())
+  
+  dir <- reactive(input$dir)
+  
+  output$dir <- renderText({
+    global$datapath
+  })
+  
+  observeEvent(ignoreNULL = TRUE,
+               eventExpr = {
+                 input$dir
+               },
+               handlerExpr = {
+                 if (!"path" %in% names(dir())) return()
+                 home <- normalizePath("~")
+                 global$datapath <-
+                   file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
+               })
 }
-shinyApp(ui=ui, server=server)
+
+# Run the application
+shinyApp(ui = ui, server = server)
